c87240738c6156843299c3f2383b376aea8573bd
demalic <demalicp@gmail.com>
2026-06-15 18:55:08 -0400

    Updated supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://bnuqtpqkddvqmmuyvgoy.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJudXF0cHFrZGR2cW1tdXl2Z295Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc3NzA1MjgsImV4cCI6MjA5MzM0NjUyOH0.FB1lpM6mYiJjywwhLwAO_0Fg4qnfkKU-F5pWIOG8vnM';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error('Missing Supabase environment variables');
+}
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
